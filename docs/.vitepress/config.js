1717431c2cf8aae8ca53fc4e3924898f60f20ba0
何洪懿 <hehongyi1997@163.com>
2024-07-01 14:03:38 +0800

    新增:英文文档

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,18 @@
+export default {
+    title: '英文文章阅读',
+    description: '英文文章阅读',
+    themeConfig: {
+        sidebar: [
+            {
+                text: '1.Leadership lessons from the prime minister of Canada',
+                link: '/2024-07-01/001'
+            },
+            // {
+            //     text: '文章二',
+            //     link: '/2024-07-02/001'
+            // }
+        ]
+    }
+}
+
+
